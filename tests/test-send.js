3e2148e888ee194b44ffbda03572a9bf74c7758e
jhickmanarc <jhickman@arcdigitalservices.com>
2024-08-18 23:31:36 -0700

    Moved test buttons to a slot

--- tests/test-send.js
+++ tests/test-send.js
@@ -7,16 +7,13 @@ class EventSenderComponent extends HTMLElement {
         this.shadowRoot.innerHTML = `
         <style></style>
         <div id='event-sender'>
-            <button data-selector="#top-component" data-value="1">Send to First</button>
-            <button data-selector="#middle-component" data-value="2">Send to Second</button>
-            <button data-selector="#bottom-component" data-value="3">Send to Third</button>
-            <button data-selector=".test-set" data-value="321">Send to Second and Third</button>
+            <slot></slot>
         </div>
         `;
     }
 
     connectedCallback() {
-        Array.from(this.shadowRoot.querySelectorAll("BUTTON")).map((el) => {
+        Array.from(this.querySelectorAll("BUTTON")).map((el) => {
             el.addEventListener('click', (e) => {
                 const el_btn = e.currentTarget;
                 const selector = el_btn.dataset.selector;
